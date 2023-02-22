02614a6d77672e5c2944b04d3876ceb2e6bca135
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-02-22 21:41:21 +0200

    1task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,21 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChange);
+
+function changeColor() {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChange() {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
