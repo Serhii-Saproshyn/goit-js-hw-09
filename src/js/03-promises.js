67093539f49fda168a9de24f6749de1b84fbdb31
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-02-26 15:16:41 +0200

    task-3 fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,17 +5,25 @@ const refs = {
   delay: document.querySelector('[name="delay"]'),
   step: document.querySelector('[name="step"]'),
   amount: document.querySelector('[name="amount"]'),
+  formBtn: document.querySelector('[type="submit"]'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
+  refs.formBtn.disabled = true;
   let delay = Number(refs.delay.value);
   for (let i = 1; i <= Number(refs.amount.value); i += 1) {
-    delay += Number(refs.step.value);
     createPromise(i, delay).then(onSuccess).catch(onError);
+    delay += Number(refs.step.value);
   }
+  refs.delay.value = '';
+  refs.step.value = '';
+  refs.amount.value = '';
+  setTimeout(() => {
+    refs.formBtn.disabled = false;
+  }, delay + 500);
 }
 
 function createPromise(position, delay) {
